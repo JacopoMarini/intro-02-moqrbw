023c1cebf220411c0ebcfaccce6052c0333c5114
JacopoMarini <106112086+JacopoMarini@users.noreply.github.com>
2022-12-06 16:44:56 +0100

    3 files modified

--- index.js
+++ index.js
@@ -2,7 +2,16 @@ import { config, products, validationRules } from './config';
 import { fieldsMap } from './fields';
 import './style.css';
 
+//Creo il titolo della pagina
+const mainTitle = document.createElement('h1');
+mainTitle.textContent = 'Hello!';
+const divEl = document.querySelector('.r-title');
+divEl.appendChild(mainTitle);
+
+
+
 const formNode = document.getElementById('dynamic-form');
+
 
 const registerButton = document.getElementById('register-button');
 
