d11628519f5bffc3d7c28f60912e09f71692a683
JacopoMarini <106112086+JacopoMarini@users.noreply.github.com>
2022-12-07 14:11:28 +0100

    4 files modified

--- fields.js
+++ fields.js
@@ -1,4 +1,19 @@
-const createSection = (section) => {};
+const createSection = (section) => {
+  const wrapperEl = document.querySelector('.section-header');
+  const sectionEl = document.createElement('div');
+  wrapperEl.appendChild(sectionEl);
+  Object.entries(section).forEach((el) => {
+    const hElement = document.createElement('h3');
+    const dElement = document.createElement('p');
+    hElement.appendChild(sectionEl);
+    dElement.appendChild(sectionEl);
+
+    el.forEach((data) => {
+      hElement.textContent = data.title;
+      dElement.textContent = data.description;
+    });
+  });
+};
 
 const createTextField = (textField, onChange) => {};
 
--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 import { config, products, validationRules } from './config';
 import { fieldsMap } from './fields';
 import './style.css';
+
 const formNode = document.getElementById('dynamic-form');
+
 const registerButton = document.getElementById('register-button');
+
+const { title, description, field } = config;
+const { label, id, type, section } = fieldsMap;
 
 //Creo il titolo della pagina
 const mainTitle = document.createElement('h1');
 mainTitle.textContent = 'Hello!';
 const divEl = document.querySelector('.r-title');
 divEl.appendChild(mainTitle);
+// -------------------------- //
+
+const x = section(config);
 
 registerButton.onclick = function onSubmit() {
   alert('prova');
